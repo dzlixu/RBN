a3a8fdcdf379997e599626a05fa74ac2a2d54adc
dzlixu <dzlixu@126.com>
2017-02-24 16:32:28 +0800

    we

---
+++ Head_First_Java/3.主要数据类型和引用/Triangle.java
@@ -0,0 +1,15 @@
+package Chapter3;
+
+public class Triangle {
+	double area;
+	int height;
+	int length;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
